abd19c2c49036c19924f0451059e887b5aa56a39
Zi Tan <tanzitian3@gmail.com>
2024-03-04 17:20:35 -0500

    handle click

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,17 @@
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo"];
 
+  const handleClick = () => {};
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No Item Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             className="list-group-item"
             key={item}
-            onClick={() => console.log("click")}
+            onClick={(e) => console.log(e)}
           >
             {item}
           </li>
