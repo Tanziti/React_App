3889beb15623579467fc438418389ed2a2a88104
Zi Tan <tanzitian3@gmail.com>
2024-03-09 20:59:54 -0500

    handle item

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import Message from "./Message";
 
 function App() {
   const items = ["New York", "San Francisco", "Tokyo"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
