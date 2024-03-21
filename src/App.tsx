e34533fe3a4391cea4524eed53838eaa37ab5927
Zi Tan <tanzitian3@gmail.com>
2024-03-21 10:28:02 -0400

    adding alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div>
+      <Alert>My Alert</Alert>
       <Button color="danger" onClick={() => console.log("clicked")}>
         My Button
       </Button>
